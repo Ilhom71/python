8b38e707bace73c6b75ac3bebbd4d413f83799b0
Ilhom71 <mirtojiyevilhom@gmail.com>
2025-03-13 20:53:03 +0500

    if op qoshdim

---
+++ if/if.py
@@ -0,0 +1,45 @@
+# car=["toyota","mazda",'huyundai','gm','kia']
+
+# for c in car:
+#     if(c!='gm'):
+#         print(c.title())
+#     else:
+#         print(c.upper())  
+
+
+# ism=input("ismingizni kiritin \t")
+
+# if(ism.lower()=='ilhom'):
+#     print(f"salom hurmatli admin {ism}")
+# else:
+#     print(f"salom huramtlai foydalanuvchi {ism}")    
+
+
+# son1=int(input("iltimos  son 1 yozin\t"))
+# son2=int(input("iltimos  son 2 yozin\t"))
+
+# if(son1==son2):
+#     print("bu sonlar tneg")
+# else:
+#     print("bu sonlar teng emas")    
+
+
+# son=int(input("istalgan son yozin manfiy yoki musbat "))
+
+
+# if(son>0):
+#     print("son musbat")
+# elif (son<0):
+#     print('bu elif bilan yozildi bu son manfiy')
+# else:
+#     print("son 0 ga teng")    
+
+
+# son=int(input("son yozin"))
+
+# if son>0:
+#     print(son**(1/2))
+# elif son<0:
+#     print("musbat son yoz mol !! bunda ildiz chiqmaydi")
+# else:
+#     print("0 yozma mol 0ni  ildiz yoq  ")    
