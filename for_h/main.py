ee5357e1de727d50bfa8de4322ba73f350374981
Ilhom71 <mirtojiyevilhom@gmail.com>
2025-03-13 09:11:45 +0500

    for bor

---
+++ for_h/main.py
@@ -0,0 +1,34 @@
+# ism=['ilhom','qodir','jasur','sardor','ali']
+# n=0;
+# for name in ism :
+
+#     print("salom" , name)
+#     n+=1
+
+# print(f'kod {len(ism)} martda takrorlandi ')
+
+# son=list(range(11,100,2))
+# for n in son  :
+#     print(n**3,'bu ',n,'ning kubi')
+
+
+
+# print("o'zingiz yoqtirgan 5 kino nomni yozin")
+# kino=[]
+
+# for name in range(1,6,1):
+#     a=input(f"{name} kinoni yoz")
+#     kino.append(a)
+
+
+# print(kino)
+
+
+# n=input('bugun nechta odam bilan uchrashdingiz\n>>')
+# n=int(n)
+# odamlar=[]
+# for i in range(1,n+1,1):
+#    a= input(f"{i} suhbat qilgan odamingiz kim:\t")
+#    odamlar.append(a)
+
+# print(odamlar)
