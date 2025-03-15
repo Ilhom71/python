3fd84bf0fc6d6c60006f043464462dc7cba1a7c0
Ilhom71 <mirtojiyevilhom@gmail.com>
2025-03-15 10:43:48 +0500

    if in and or not in

---
+++ If_2/if.py
@@ -0,0 +1,93 @@
+# son=int(input("son yozin \t "))
+
+# if (son%2==0 and son!=0):
+#     print("raxmat")
+# elif son%2!=0:
+#     print("bu son juft emas")    
+# else :
+#     print("0 yozma ")
+
+
+##############################################################################################################################33
+# yosh = int ( input ( " yoshingizni yozin"))
+
+# if yosh<=4 and yosh>=60:
+#     print(" siz ga bepul")
+# elif  yosh<=18 and yosh>=4:
+#     print("sizga 10,000 so'm")
+# else:
+#     print('sizga 20000 sum')
+
+#############################################################################################################################3
+# son1=int(input("birinchi son yoz \t"))
+# son2=int(input("ikkinchi son yoz\t"))
+
+
+# if son1>son2:
+#     print(son1,'>',son2)
+# elif son1<son2:
+#     print(son1,'<',son2)    
+# else:
+#     print("teng")
+
+###################################################################################################################
+# mahsulotlar=["olma","anor","nok","o'rik","gilos","go'sh","non","un","guruch","sabzi","piyoz","yog'","kartoshka"]
+
+# haridlar=[]
+
+# for n in range(1,6,1):
+#     s=input(f"{n} mahsulotni kiritin >>")
+#     haridlar.append(s)
+
+# for m in haridlar:
+#     if m in mahsulotlar:
+#         print(f"do'konda bu {m} bor")
+#     else:
+#         print(f"do'konda bu {m} yoq")    
+
+
+###############################################################################################################
+
+# mahsulotlar=["olma","anor","nok","o'rik","gilos","go'sht","non","un","guruch","sabzi","piyoz","yog","kartoshka"]
+# haridlar=[]
+# yoq =[]
+# for n in range(1,6):
+#   s=input(f"{n} mahsulotni kiritin >>\t").lower()
+#   haridlar.append(s)
+
+# print(haridlar)
+
+
+
+# for h in haridlar:
+#   if h in mahsulotlar:
+#     print (f" bu {h} bor")
+#   else:
+#     yoq.append(h)  
+
+# print("bular yoq",yoq)
+ 
+################################################################################################################  
+
+
+# foydalanuvchi =["ilhom","jasur","ali","umar","asil"]
+
+
+# name = str(input("yangi login  yozin>>"))
+
+# if name.lower() in foydalanuvchi:
+#   print("bu login band")
+# else:
+#   print ("xush kelibsiz")  
+
+
+####################################################################################################################
+
+
+# num = int(input("butun son yoz"))
+
+
+# for n in range(2,11):
+#   if num%n==0:
+#     print(f"bu {n} -> {num } qoldiqsiz bo'linadi")
+
