84f2fd0eeb41479bcba6726f0bd07489e52b7b82
Ilhom71 <mirtojiyevilhom@gmail.com>
2025-03-23 14:28:13 +0500

    function 4 qo'shildi unda *arg va textga **kwg bor ishlash arg function solar uchun cheksiz parametr **kvg es text uchun yozilish key=value bo'ladi songa *son matnga **matn

---
+++ function_4/funct.py
@@ -0,0 +1,27 @@
+
+# def inf(*son):
+#    y=1
+#    for s in son:
+#        y*=s
+
+#    return y   
+    
+
+    
+# a=inf(1,2,4,5)
+# print(a)    
+
+
+###############################################################################################33
+
+
+# def oj(ism,familiya,**m):
+    
+#     m["ism"]=ism
+#     m["familiya"]=familiya
+    
+#     return m
+
+
+# peroson=oj("ilhom","mirtojiyev",yil=2004,yosh=21,joy="toshkent")
+# print(peroson)
