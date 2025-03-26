39fdabd29dd0323ac3e5b9624bcd6f51ac9b9a93
Ilhom71 <mirtojiyevilhom@gmail.com>
2025-03-26 10:39:36 +0500

    funkist 6ni qo'shdim bu oxirgisi bu lambda va map filter bor

---
+++ function_6/main.py
@@ -0,0 +1,71 @@
+# from math import *
+# from math import random as r
+# a=lambda name:f"salom {name}"
+# print(a("ilhom"))
+
+#################################################################################################################################
+
+# even = lambda n : n%2==0
+
+# print(even(1))
+#################################################################################################################################
+
+# son=int(input("son yoz :"))
+# a=lambda n:n**2
+# print(a(son))
+#################################################################################################################################
+
+
+# sonlar=list(range(11))
+# ildizlar=list(map(sqrt,sonlar))
+# print(ildizlar)
+###################################################
+# sonlar = list(range(11))
+
+# a=lambda n:pow(n,2) 
+
+# for val in sonlar:
+#     print(a(val))    
+######################################################
+
+# sonlar = list(range(11))
+# kv=list(map(lambda s:s*s,sonlar))
+# print(kv)
+
+########################################################
+
+# son1=[1,2,3]
+# son2=[4,5,6]
+
+# plus=list(map(lambda x,y:x+y,son1,son2))
+# print(plus)
+
+
+#########################################################
+
+
+# ismlar=["ilhom","ali","jahongir","bekzod","alisher","temur","bobur"]
+
+# kattam=list(map(lambda name:name.title(),ismlar))
+
+# print(kattam)
+
+#############################################################
+# import random 
+
+# sonlar = random.sample(range(100),10)
+# print(sonlar)
+# juft=list(filter(lambda x:x%2==0,sonlar))
+# print(juft)
+
+#################################################################
+
+ismlar = ["ilhom","ali","bek","boyboy","name","qo","i","ajkj"]
+# first_word = list(filter(lambda name :name.startswith('b'),ismlar))
+# print(first_word)
+ 
+# last_word=list(filter(lambda name:name.endswith("e"),ismlar))
+# print(last_word)
+
+# tow_words=list(filter(lambda name:name.startswith("i") and  name.endswith("m"),ismlar))
+# print(tow_words)
